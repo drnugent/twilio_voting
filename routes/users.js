ec182e3d3ff2007453ddb2f4493842949e8eed38
Chuks Opia <chuks24ng@yahoo.co.uk>
2020-02-09 16:03:26 +0100

    Update chart from route response

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const voteCount = {
+  football: 0,
+  basketball: 0,
+  cricket: 0
+};
+
+/* POST vote for nominee. */
+router.post('/', function (req, res, next) {
+
+  const key = Object.keys(req.body)[0];
+  voteCount[key]++;
+  res.status(200).send(voteCount);
 });
 
 module.exports = router;
