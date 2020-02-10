61764ff985fd7b14c8767f4c0484b93f63a2f002
Chuks Opia <chuks24ng@yahoo.co.uk>
2020-02-10 13:26:23 +0100

    Update chart in realtime with Sync

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Twilio = require("twilio");
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const apiKey = process.env.TWILIO_API_KEY;
+const apiSecret = process.env.TWILIO_API_SECRET;
+const syncServiceSid = process.env.TWILIO_SYNC_SERVICE_SID || "default";
+
+const client = new Twilio(accountSid, authToken);
 
 const voteCount = {
   basketball: 0,
@@ -8,10 +17,17 @@ const voteCount = {
 };
 
 /* POST vote for nominee. */
-router.post('/', function (req, res, next) {
-
+router.post("/", function(req, res, next) {
   const key = Object.keys(req.body)[0];
   voteCount[key]++;
+
+  // update data in Sync document
+  client.sync
+    .services(syncServiceSid)
+    .documents("SportsPoll")
+    .update({ data: voteCount })
+    .then(document => console.log(document));
+
   res.status(200).send(voteCount);
 });
 
